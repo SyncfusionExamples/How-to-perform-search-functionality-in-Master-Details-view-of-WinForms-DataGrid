02ca80b1491de18c231398dddbf96ffe88188c34
Rabina4363sf <rabina.murugan@syncfusion.com>
2025-08-11 17:58:50 +0530

    KB-20881 - Sample and README.md updated

---
+++ SfDataGridDemo/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace SfDataGridDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sfDataGrid1 = new Syncfusion.WinForms.DataGrid.SfDataGrid();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.sfDataGrid1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sfDataGrid1
+            // 
+            this.sfDataGrid1.AccessibleName = "Table";
+            this.sfDataGrid1.Location = new System.Drawing.Point(16, 15);
+            this.sfDataGrid1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.sfDataGrid1.Name = "sfDataGrid1";
+            this.sfDataGrid1.PreviewRowHeight = 35;
+            this.sfDataGrid1.Size = new System.Drawing.Size(945, 767);
+            this.sfDataGrid1.TabIndex = 0;
+            this.sfDataGrid1.Text = "sfDataGrid1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(1019, 291);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(267, 123);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search here";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(156, 78);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Next";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.nextButtonClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 78);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Previous";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.previousButtonClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 23);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.OnTextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1347, 795);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.sfDataGrid1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.sfDataGrid1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Syncfusion.WinForms.DataGrid.SfDataGrid sfDataGrid1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SfDataGridDemo/Form1.cs
@@ -0,0 +1,131 @@
+﻿using Syncfusion.WinForms.DataGrid;
+using Syncfusion.WinForms.DataGrid.Events;
+using Syncfusion.WinForms.DataGrid.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SfDataGridDemo
+{
+    public partial class Form1 : Form
+    {
+        SfDataGrid detailsViewGrid;
+        public Form1()
+        {
+            InitializeComponent();
+            SampleCustomization();
+            this.Load += OnLoad;
+        }
+
+        /// <summary>
+        /// Occurs when the form is loading.
+        /// </summary>
+        /// <param name="sender">The sender of event.</param>
+        /// <param name="e">The <see cref="T:System.EventArgs"/> that contains event data.</param>
+        private void OnLoad(object sender, EventArgs e)
+        {
+            this.sfDataGrid1.ExpandAllDetailsView();
+        }
+
+        /// <summary>
+        /// Sets the sample customization settings.
+        /// </summary>
+        private void SampleCustomization()
+        {           
+            this.sfDataGrid1.AutoGenerateRelations = true;           
+            this.sfDataGrid1.AutoGeneratingRelations += OnAutoGeneratingRelations;
+            var parentTable = GetParentDataTable();
+            var childTable = GetChildDataTable();
+            DataSet dataSet = new DataSet();
+            dataSet.Tables.Add(parentTable);
+            dataSet.Tables.Add(childTable);
+            dataSet.Relations.Add(new DataRelation("Parent_Child", dataSet.Tables[0].Columns["ID"], dataSet.Tables[1].Columns["ID"]));
+            this.sfDataGrid1.DataSource = dataSet.Tables[0];            
+        }
+
+        private void OnAutoGeneratingRelations(object sender, AutoGeneratingRelationsEventArgs e)
+        {
+            // To get the DeatilsView dataGrid
+            detailsViewGrid = e.GridViewDefinition.DataGrid;
+        }
+        
+        DataTable GetParentDataTable()
+        {
+            DataTable collection = new DataTable();
+            collection.Columns.Add("ID", typeof(int));
+            collection.Columns.Add("Name", typeof(string));
+            collection.Columns.Add("Q1", typeof(float));
+            collection.Columns.Add("Q2", typeof(float));
+            collection.Columns.Add("Q3", typeof(float));
+            collection.Rows.Add(1001, "Belgim", 872.81, 978.89, 685.90);
+            collection.Rows.Add(1002, "Oliver", 978.76, 458.21, 675.99);
+            collection.Rows.Add(1003, "Bernald", 548.31, 234.32, 423.44);
+            collection.Rows.Add(1004, "James", 123.31, 6543.12, 978.31);
+
+            return collection;
+        }
+
+        DataTable GetChildDataTable()
+        {
+            DataTable collection = new DataTable();
+            collection.Columns.Add("ID", typeof(int));
+            collection.Columns.Add("Name", typeof(string));
+            collection.Columns.Add("City", typeof(string));
+            collection.Columns.Add("Quantity", typeof(int));
+            collection.Columns.Add("UnitPrice", typeof(int));
+            collection.Rows.Add(1001, "Belgim", "California", 10, 50);
+            collection.Rows.Add(1001, "Belgim", "Colorado", 20, 35);
+            collection.Rows.Add(1001, "Belgim", "Alaska", 50, 65);
+            collection.Rows.Add(1001, "Belgim", "Roraima", 10, 40);
+
+            collection.Rows.Add(1002, "Oliver", "California", 32, 40);
+            collection.Rows.Add(1002, "Oliver", "Alaska", 24, 35);
+            collection.Rows.Add(1002, "Oliver", "Roraima", 98, 50);
+            collection.Rows.Add(1002, "Oliver", "Colorado", 78, 65);
+
+            collection.Rows.Add(1003, "Bernald", "California", 89, 35);
+            collection.Rows.Add(1003, "Bernald", "Alaska", 10, 65);
+            collection.Rows.Add(1003, "Bernald", "Colorado", 20, 50);
+            collection.Rows.Add(1003, "Bernald", "Roraima", 30, 40);
+
+            collection.Rows.Add(1004, "James", "Colorado", 22, 50);
+            collection.Rows.Add(1004, "James", "Roraima", 53, 40);
+            collection.Rows.Add(1004, "James", "California", 65, 65);
+            collection.Rows.Add(1004, "James", "Alaska", 25, 35);
+
+            return collection;
+        }
+
+        private void OnTextChanged(object sender, EventArgs e)
+        {
+            // To perform search in DetailsView DataGrid
+            detailsViewGrid.SearchController.Search(this.textBox1.Text);
+        }
+
+        private void previousButtonClick(object sender, EventArgs e)
+        {
+            // To navigate the previous cell match with search text
+            if (detailsViewGrid.SearchController.FindPrevious(this.textBox1.Text))
+            {
+                var detailsViewGrid = sfDataGrid1.GetDetailsViewGrid(this.sfDataGrid1.SearchController.CurrentRowColumnIndex.RowIndex);
+                detailsViewGrid.MoveToCurrentCell(detailsViewGrid.SearchController.CurrentRowColumnIndex);
+            }            
+        }
+
+        private void nextButtonClick(object sender, EventArgs e)
+        {
+            // To navigate the next cell match with search text
+            if (detailsViewGrid.SearchController.FindNext(this.textBox1.Text))
+            {
+                var detailsViewGrid = sfDataGrid1.GetDetailsViewGrid(this.sfDataGrid1.SearchController.CurrentRowColumnIndex.RowIndex);
+                detailsViewGrid.MoveToCurrentCell(detailsViewGrid.SearchController.CurrentRowColumnIndex);
+            }
+        }
+    }
+}
